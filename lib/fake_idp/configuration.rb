a8eb04379745fc54e77080fdf1e031f86446435f
Tyler Willingham <tylerwillingham@gmail.com>
2018-10-16 11:50:24 -0600

    Expose the x509 certificate and secret key from SamlIdp in FakeIdp.configuration

--- lib/fake_idp/configuration.rb
+++ lib/fake_idp/configuration.rb
@@ -1,6 +1,15 @@
 module FakeIdp
   class Configuration
-    attr_accessor :callback_url, :sso_uid, :username, :first_name, :last_name, :email, :name_id
+    attr_accessor(
+      :callback_url,
+      :sso_uid,
+      :username,
+      :first_name,
+      :last_name,
+      :email,
+      :name_id,
+    )
+
     def initialize
       @callback_url = ENV['CALLBACK_URL']
       @sso_uid = ENV['SSO_UID']
@@ -9,6 +18,14 @@ module FakeIdp
       @last_name = ENV['LAST_NAME']
       @email = ENV['EMAIL']
       @name_id = ENV['NAME_ID']
+    end
+
+    def x509_certificate
+      SamlIdp::Default::X509_CERTIFICATE
+    end
+
+    def secret_key
+      SamlIdp::Default::SECRET_KEY
     end
   end
 end
